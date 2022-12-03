51e00b7a65b46ebe8fb1f19d864ef7ee36c09915
Marco Antonio <maxter_anime12@outlook.com>
2022-12-03 09:12:17 -0500

    ingresos

--- src/main/java/org/cibertec/controlador/UsuarioControlador.java
+++ src/main/java/org/cibertec/controlador/UsuarioControlador.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.cibertec.entidad.Usuario;
 import org.cibertec.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Usuario")
+@CrossOrigin("*")
 public class UsuarioControlador {
 
 	@Autowired
